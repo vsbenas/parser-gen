de0bae341f7b6d5254ea5493a92422a910897199
vsbenas <vsbenas@gmail.com>
2017-06-07 15:02:02 +0100

    Function description for peg-parse
    Will definitely be updated in the future, but good initial reference.

--- peg-parser.lua
+++ peg-parser.lua
@@ -8,8 +8,31 @@ local space = lpeg.space
 local alpha = lpeg.alpha
 
 
-local grammar = P (1)
+local grammar = P {
+	"program",
+	program = P(1),
+}
 
+--[[
+Function: parse(input)
+
+Input: a grammar in PEG format, described in https://github.com/vsbenas/parser-gen
+
+Output: if parsing successful - a table of grammar rules, else - runtime error
+
+Example input: "Program <- stmt* / SPACE;
+				stmt <- ('a' / 'b')+;
+				SPACE <- '';"
+Example output: {
+	{rulename = "Program", 	rule = {action = "or", op1 = {action = "zero-or-more", op1 = "stmt"}, op2 = "SPACE"}},
+	{rulename = "stmt", 	rule = {action = "one-or-more", op1 = {action="or", op1 = "'a'", op2 = 'b'}},
+	{rulename = "SPACE",	rule = "''", token=1},
+
+}
+
+The rules are further processed and turned into lpeg compatible format in parser-gen.lua
+
+]]--
 function peg.parse(input)
 	return input
 end
