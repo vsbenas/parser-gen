c241f2213928d1e464f10fc933d1f03164ef295b
vsbenas <vsbenas@gmail.com>
2017-06-09 19:49:05 +0100

    added a function to handle inputs and command line support

--- parsers/tiny-parser.lua
+++ parsers/tiny-parser.lua
@@ -1,30 +1,42 @@
 pg = require "parser-gen"
 grammar = [[
-program <- stmt-sequence EOF;
-stmt-sequence <- statement (';' statement)*;
-statement <- if-stmt / repeat-stmt / assign-stmt / read-stmt / write-stmt;
-if-stmt <- 'if' exp 'then'^{errMissingThen} stmt-sequence ('else' stmt-sequence)? 'end';
-repeat-stmt <- 'repeat' stmt-sequence 'until' exp;
-assign-stmt <- identifier ':=' exp;
-read-stmt <- 'read' identifier;
-write-stmt <- 'write' exp;
-exp <- simple-exp (COMPARISON-OP simple-exp)*;
-COMPARISON-OP <- '<' / '=';
-simple-exp <- term (ADD-OP term)*;
-ADD-OP <- '+' / '-';
-term <- factor (MUL-OP factor)*;
-MUL-OP <- '*' / '/';
-factor <- '(' exp ')' / NUMBER / IDENTIFIER;
 
-NUMBER <- '-'? [09]+;
-IDENTIFIER <- ([az] / [AZ])+;
+  program <- stmt-sequence EOF;
+  stmt-sequence <- statement (';' statement)*;
+  statement <- if-stmt / repeat-stmt / assign-stmt / read-stmt / write-stmt;
+  if-stmt <- 'if' exp 'then'^{errMissingThen} stmt-sequence ('else' stmt-sequence)? 'end';
+  repeat-stmt <- 'repeat' stmt-sequence 'until' exp;
+  assign-stmt <- identifier ':=' exp;
+  read-stmt <- 'read' identifier;
+  write-stmt <- 'write' exp;
+  exp <- simple-exp (COMPARISON-OP simple-exp)*;
+  COMPARISON-OP <- '<' / '=';
+  simple-exp <- term (ADD-OP term)*;
+  ADD-OP <- '+' / '-';
+  term <- factor (MUL-OP factor)*;
+  MUL-OP <- '*' / '/';
+  factor <- '(' exp ')' / NUMBER / IDENTIFIER;
 
-SYNC <- ';' / '\n' / '\r';
+  NUMBER <- '-'? [09]+;
+  IDENTIFIER <- ([az] / [AZ])+;
+
+  SYNC <- ';' / '\n' / '\r';
 
 ]]
 
-function printerror(label,error,line,col)
+local function printerror(label,error,line,col)
   print("Error #"..label..": "..error.." on line "..line.."(col "..col..")")
 end
-input = "a:=1; if b=3 then c else d end"
-result = pg.parse(input,grammar,printerror)
+
+ret = {}
+
+local function ret.parse(input)
+  result, errors = pg.parse(input,grammar,printerror)
+end
+
+if arg[1] then	
+	-- argument must be in quotes if it contains spaces
+	ret.parse(arg[1])
+end
+
+return ret
