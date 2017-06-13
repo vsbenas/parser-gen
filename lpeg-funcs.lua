f6c7e6d6bdfaf9226e50aabcc18f23b63e8cb476
vsbenas <vsbenas@gmail.com>
2017-06-13 16:07:12 +0100

    added print table function for testing

--- lpeg-funcs.lua
+++ lpeg-funcs.lua
@@ -38,4 +38,30 @@ function lpeg.throws (patt,err) -- if pattern is matched throw error
 	return patt * T(err)
 end
 
+
+function lpeg.print_r ( t )  -- for debugging
+    local print_r_cache={}
+    local function sub_print_r(t,indent)
+        if (print_r_cache[tostring(t)]) then
+            print(indent.."*"..tostring(t))
+        else
+            print_r_cache[tostring(t)]=true
+            if (type(t)=="table") then
+                for pos,val in pairs(t) do
+                    if (type(val)=="table") then
+                        print(indent.."["..pos.."] => "..tostring(t).." {")
+                        sub_print_r(val,indent..string.rep(" ",string.len(pos)+8))
+                        print(indent..string.rep(" ",string.len(pos)+6).."}")
+                    else
+                        print(indent.."["..pos.."] => "..tostring(val))
+                    end
+                end
+            else
+                print(indent..tostring(t))
+            end
+        end
+    end
+    sub_print_r(t,"  ")
+end
+
 return lpeg
