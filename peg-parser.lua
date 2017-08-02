5714410f00c98884972ee9f9ddf40a2b214bcc29
vsbenas <vsbenas@gmail.com>
2017-08-02 01:29:00 +0100

    new print table function

--- peg-parser.lua
+++ peg-parser.lua
@@ -207,5 +207,33 @@ function peg.print_r ( t )  -- for debugging
     end
     sub_print_r(t,"")
 end
+function peg.print_t ( t )  -- for debugging
+    local print_r_cache={}
+    local function sub_print_r(t,indent)
+        if (print_r_cache[tostring(t)]) then
+            print(indent.."*"..tostring(t))
+        else
+            print_r_cache[tostring(t)]=true
+            if (type(t)=="table") then
+                for pos,val in pairs(t) do
+                    if (type(val)=="table") then
+                        print(indent.."{")
+                        sub_print_r(val,indent..string.rep(" ",string.len(pos)+8))
+                        print(indent..string.rep(" ",string.len(pos)+6).."},")
+                    else
+						if tonumber(pos) then
+							print(indent.."'"..tostring(val).."',")
+						else
+							print(indent..pos.."='"..tostring(val).."',")
+						end
+                    end
+                end
+            else
+                print(indent..tostring(t))
+            end
+        end
+    end
+    sub_print_r(t,"")
+end
 
 return peg
